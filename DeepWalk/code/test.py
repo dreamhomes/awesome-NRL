7208e5a04f66559c8d570789985cded26a4152f1
dreamhomes <2284672469@qq.com>
2019-12-02 18:00:40 +0800

    update

---
+++ DeepWalk/code/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+'''
+@date: 2019-12-02
+ 
+@author: dreamhomes
+ 
+@description：test file
+'''
+f = open("data/cora/cora.content", 'r')
+for line in f.readlines():
+    print(line)
